9efc3cf6f073217b826278b22cb474f8df553771
Lei Guo <leguo@google.com>
2017-09-28 23:11:18 -0700

    update gnmi get client: add xpath and pbpath options
    supporting querying multiple paths, including xpath format and proto
    format

--- gnmi_get/gnmi_get.go
+++ gnmi_get/gnmi_get.go
@@ -13,30 +13,54 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Binary gnmi_get performs a get request against a gNMI Target.
+// Binary gnmi_get performs a get request against a gnmi target.
 package main
+
+// Typical usage:
+// go run gnmi_get.go \
+//		-xpath "/system/openflow/agent/config/datapath-id" \
+//		-xpath "/system/openflow/agent/config/backoff-interval" \
+//		-target_addr localhost:10161 -target_name www.example.com \
+//		-ca ca.crt -cert client.crt -key client.key \
+//		-username foo -password bar
 
 import (
 	"flag"
 	"fmt"
+	"strings"
 	"time"
 
 	log "github.com/golang/glog"
 	"github.com/golang/protobuf/proto"
-	"github.com/google/gnxi/client"
-	"github.com/google/gnxi/credentials"
 	"github.com/kylelemons/godebug/pretty"
-	"github.com/openconfig/gnmi/proto/gnmi"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+
+	"github.com/google/gnxi/credentials"
+	"github.com/google/gnxi/utils/xpath"
+
+	pb "github.com/openconfig/gnmi/proto/gnmi"
 )
 
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return "my string representation"
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
 var (
-	targetAddress = flag.String("target_address", "localhost:32123", "The target address:port.")
-	targetName    = flag.String("target_name", "", "Will use this hostname to verify server certificate during TLS handshake.")
-	timeOut       = flag.Duration("time_out", 10*time.Second, "Timeout for the Get request, 10 seconds by default.")
-	query         = flag.String("query", "", "XPath query or queries. Example: system/openflow/controllers/controller[main]/connections/connection[0]/state/address")
-	usePretty     = flag.Bool("pretty", false, "Shows PROTOs using Pretty package instead of PROTO Text Marshal.")
+	xPathFlags   arrayFlags
+	pbPathFlags  arrayFlags
+	targetAddr   = flag.String("target_addr", "localhost:10161", "The target address in the format of host:port")
+	targetName   = flag.String("target_name", "www.example.com", "The target name use to verify the hostname returned by TLS handshake")
+	timeOut      = flag.Duration("time_out", 10*time.Second, "Timeout for the Get request, 10 seconds by default")
+	encodingName = flag.String("encoding", "JSON_IETF", "value encoding format to be used")
+	usePretty    = flag.Bool("pretty", false, "Shows PROTOs using Pretty package instead of PROTO Text Marshal")
 )
 
 func display(m proto.Message) {
@@ -48,30 +72,54 @@ func display(m proto.Message) {
 }
 
 func main() {
+	flag.Var(&xPathFlags, "xpath", "xpath of the config node to be fetched")
+	flag.Var(&pbPathFlags, "pbpath", "protobuf format path of the config node to be fetched")
 	flag.Parse()
 
-	if *query == "" {
-		log.Exit("-query must be set")
-	}
-	queries := client.ParseQuery(*query)
-	getRequest, err := client.ToGetRequest(queries)
+	opts := credentials.ClientCredentials(*targetName)
+	conn, err := grpc.Dial(*targetAddr, opts...)
 	if err != nil {
-		log.Exitf("error generating GetRequest: %v", err)
-	}
-
-	conn, err := grpc.Dial(*targetAddress, credentials.ClientCredentials(*targetName)...)
-	if err != nil {
-		log.Exitf("did not connect: %v", err)
+		log.Exitf("Dialing to %q failed: %v", *targetAddr, err)
 	}
 	defer conn.Close()
-	c := gnmi.NewGNMIClient(conn)
+
+	cli := pb.NewGNMIClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), *timeOut)
 	defer cancel()
 
-	getResponse, err := c.Get(ctx, getRequest)
+	encoding, ok := pb.Encoding_value[*encodingName]
+	if !ok {
+		var gnmiEncodingList []string
+		for _, name := range pb.Encoding_name {
+			gnmiEncodingList = append(gnmiEncodingList, name)
+		}
+		log.Exitf("Supported encodings: %s", strings.Join(gnmiEncodingList, ", "))
+	}
+
+	var pbPathList []*pb.Path
+	for _, xPath := range xPathFlags {
+		pbPath, err := xpath.ToGNMIPath(xPath)
+		if err != nil {
+			log.Exitf("error in parsing xpath %q to gnmi path", xPath)
+		}
+		pbPathList = append(pbPathList, pbPath)
+	}
+	for _, textPbPath := range pbPathFlags {
+		var pbPath pb.Path
+		if err := proto.UnmarshalText(textPbPath, &pbPath); err != nil {
+			log.Exitf("error in unmarshaling %q to gnmi Path", textPbPath)
+		}
+		pbPathList = append(pbPathList, &pbPath)
+	}
+
+	getRequest := &pb.GetRequest{
+		Encoding: pb.Encoding(encoding),
+		Path:     pbPathList,
+	}
+	getResponse, err := cli.Get(ctx, getRequest)
 	if err != nil {
-		log.Exitf("could not get: %v", err)
+		log.Exitf("fetch config from the path failed: %v", err)
 	}
 
 	fmt.Println("== getRequest:")
