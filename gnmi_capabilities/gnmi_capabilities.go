7799c34e759e64a718c3de01d5675e1e53e40aba
Lei Guo <leguo@google.com>
2017-09-28 23:00:30 -0700

    add gnmi capabilities client

---
+++ gnmi_capabilities/gnmi_capabilities.go
@@ -0,0 +1,68 @@
+/* Copyright 2017 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package gnmi_capabilities gets the encoding and model list that the target
+// supports.
+package main
+
+// Typical usage:
+// go run gnmi_capabilities.go
+//		-target_addr localhost:10161 -target_name www.example.com \
+//		-ca ca.pem -cert client_cert.pem -key client_key.pem
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	log "github.com/golang/glog"
+	"github.com/golang/protobuf/proto"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	"github.com/google/gnxi/credentials"
+
+	pb "github.com/openconfig/gnmi/proto/gnmi"
+)
+
+var (
+	targetAddr = flag.String("target_addr", "localhost:10161", "The target address in the format of host:port")
+	targetName = flag.String("target_name", "www.example.com", "The target name use to verify the hostname returned by TLS handshake")
+	timeOut    = flag.Duration("time_out", 10*time.Second, "Timeout for the Get request, 10 seconds by default.")
+	usePretty  = flag.Bool("pretty", false, "Shows PROTOs using Pretty package instead of PROTO Text Marshal.")
+)
+
+func main() {
+	flag.Parse()
+	opts := credentials.ClientCredentials(*targetName)
+	conn, err := grpc.Dial(*targetAddr, opts...)
+	if err != nil {
+		log.Exitf("Dialing to %q failed: %v", *targetAddr, err)
+	}
+	defer conn.Close()
+
+	cli := pb.NewGNMIClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), *timeOut)
+	defer cancel()
+
+	capResponse, err := cli.Capabilities(ctx, &pb.CapabilityRequest{})
+	if err != nil {
+		log.Exitf("error in getting capabilities: %v", err)
+	}
+
+	fmt.Println("== capabilitiesResponse:")
+	fmt.Println(proto.MarshalTextString(capResponse))
+}
