40cd84ee26a3af110ccbec7533e1051f5098ecae
Sam Ribeiro <ribeiro@google.com>
2018-07-09 14:55:27 +0200

    gNOI CertManager tests.

--- gnoi/cert.go
+++ gnoi/cert.go
@@ -1,6 +1,8 @@
 package gnoi
 
 import (
+	"crypto"
+	"crypto/tls"
 	"crypto/x509"
 	"fmt"
 	"sync"
@@ -11,24 +13,26 @@ import (
 	pb "github.com/google/gnxi/gnoi/certpb"
 )
 
+// CertInterface does blah.
 type CertInterface interface {
-	Lock(string) error
-	Unlock(string)
 	GenCSR(*pb.CSRParams) (*pb.CSR, error)
 	Get() ([]*pb.CertificateInfo, error)
 	Install(*pb.LoadCertificateRequest) error
 	Revoke(*pb.RevokeCertificatesRequest) (*pb.RevokeCertificatesResponse, error)
-	Rotate(*pb.LoadCertificateRequest) (func(), error)
+	Rotate(*pb.LoadCertificateRequest) (func(), func(), error)
 }
 
+// CertServer does blah.
 type CertServer struct {
 	certInterface CertInterface
 }
 
+// NewCertServer does blah.
 func NewCertServer(cm CertInterface) *CertServer {
 	return &CertServer{certInterface: cm}
 }
 
+// Rotate does blah.
 func (s *CertServer) Rotate(stream pb.CertificateManagement_RotateServer) error {
 	var resp *pb.RotateCertificateRequest
 	var err error
@@ -40,11 +44,6 @@ func (s *CertServer) Rotate(stream pb.CertificateManagement_RotateServer) error 
 	if genCSRRequest == nil {
 		return fmt.Errorf("expected GenerateCSRRequest, got something else")
 	}
-
-	if err := s.certInterface.Lock(genCSRRequest.CertificateId); err != nil {
-		return fmt.Errorf("there is already an ongoing operation with this Certificate ID: %v", err)
-	}
-	defer s.certInterface.Unlock(genCSRRequest.CertificateId)
 
 	csr, err := s.certInterface.GenCSR(genCSRRequest.CsrParams)
 	if err != nil {
@@ -67,12 +66,12 @@ func (s *CertServer) Rotate(stream pb.CertificateManagement_RotateServer) error 
 		return fmt.Errorf("expected LoadCertificateRequest, got something else")
 	}
 
-	rotateBack, err := s.certInterface.Rotate(loadCertificateRequest)
+	rotateAccept, rotateBack, err := s.certInterface.Rotate(loadCertificateRequest)
 	if err != nil {
 		return fmt.Errorf("failed to load the Certificate: %v", err)
 	}
 
-	if err := stream.Send(&pb.RotateCertificateResponse{
+	if err = stream.Send(&pb.RotateCertificateResponse{
 		RotateResponse: &pb.RotateCertificateResponse_LoadCertificate{
 			LoadCertificate: &pb.LoadCertificateResponse{},
 		},
@@ -86,12 +85,15 @@ func (s *CertServer) Rotate(stream pb.CertificateManagement_RotateServer) error 
 	}
 	finalize := resp.GetFinalizeRotation()
 	if finalize == nil {
+		rotateBack()
 		return fmt.Errorf("expected FinalizeRequest, got something else")
 	}
+	rotateAccept()
 
 	return nil
 }
 
+// Install does blah.
 func (s *CertServer) Install(stream pb.CertificateManagement_InstallServer) error {
 	var resp *pb.InstallCertificateRequest
 	var err error
@@ -103,11 +105,6 @@ func (s *CertServer) Install(stream pb.CertificateManagement_InstallServer) erro
 	if genCSRRequest == nil {
 		return fmt.Errorf("expected GenerateCSRRequest, got something else")
 	}
-
-	if err := s.certInterface.Lock(genCSRRequest.CertificateId); err != nil {
-		return fmt.Errorf("there is already an ongoing operation with this Certificate ID: %v", err)
-	}
-	defer s.certInterface.Unlock(genCSRRequest.CertificateId)
 
 	csr, err := s.certInterface.GenCSR(genCSRRequest.CsrParams)
 	if err != nil {
@@ -143,71 +140,117 @@ func (s *CertServer) Install(stream pb.CertificateManagement_InstallServer) erro
 	return nil
 }
 
+// GetCertificates does blah.
 func (s *CertServer) GetCertificates(ctx context.Context, request *pb.GetCertificatesRequest) (*pb.GetCertificatesResponse, error) {
 	certInfo, err := s.certInterface.Get()
 	return &pb.GetCertificatesResponse{CertificateInfo: certInfo}, err
 }
 
+// RevokeCertificates does blah.
 func (s *CertServer) RevokeCertificates(ctx context.Context, request *pb.RevokeCertificatesRequest) (*pb.RevokeCertificatesResponse, error) {
 	return s.certInterface.Revoke(request)
 }
 
+// CanGenerateCSR does blah.
 func (s *CertServer) CanGenerateCSR(ctx context.Context, request *pb.CanGenerateCSRRequest) (*pb.CanGenerateCSRResponse, error) {
 	return &pb.CanGenerateCSRResponse{CanGenerate: request.KeyType == pb.KeyType_KT_RSA && request.CertificateType == pb.CertificateType_CT_X509 && request.KeySize >= 128}, nil
 }
 
+// CertManager does blah.
 type CertManager struct {
+	privateKey crypto.PrivateKey
+
 	certs        map[string]*x509.Certificate
 	certsModTime map[string]time.Time
-	muCerts      sync.Mutex
+	caBundle     []*x509.Certificate
+	mu           sync.Mutex
 
-	caBundle   []*x509.Certificate
-	muCABundle sync.Mutex
-
-	lock   map[string]bool
+	locks  map[string]bool
 	muLock sync.Mutex
 }
 
-func NewCertManager() *CertManager {
+// NewCertManager does blah.
+func NewCertManager(p crypto.PrivateKey) *CertManager {
 	return &CertManager{
+		privateKey:   p,
 		certs:        map[string]*x509.Certificate{},
 		certsModTime: map[string]time.Time{},
 		caBundle:     []*x509.Certificate{},
-		lock:         map[string]bool{},
+		locks:        map[string]bool{},
 	}
 }
 
-func (cm *CertManager) Lock(certID string) error {
+func toSlices(certs []*pb.Certificate) [][]byte {
+	ret := [][]byte{}
+	for _, cert := range certs {
+		ret = append(ret, cert.Certificate)
+	}
+	return ret
+}
+
+var nowTime = time.Now
+
+// Certificates does blah.
+func (cm *CertManager) Certificates() ([]*tls.Certificate, []*x509.Certificate) {
+	certs := []*tls.Certificate{}
+	caBundle := []*x509.Certificate{}
+
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
+	for _, cert := range cm.certs {
+		certs = append(certs, &tls.Certificate{
+			Leaf:        cert,
+			Certificate: [][]byte{cert.Raw},
+			PrivateKey:  cm.privateKey,
+		})
+	}
+	for _, cert := range cm.caBundle {
+		caBundle = append(caBundle, cert)
+	}
+	return certs, caBundle
+}
+
+func (cm *CertManager) locked(certID string) bool {
 	cm.muLock.Lock()
 	defer cm.muLock.Unlock()
-	if _, ok := cm.lock[certID]; ok {
+	_, ok := cm.locks[certID]
+	return ok
+}
+
+func (cm *CertManager) lock(certID string) error {
+	cm.muLock.Lock()
+	defer cm.muLock.Unlock()
+	if _, ok := cm.locks[certID]; ok {
 		return fmt.Errorf("an operation with certID %q is already in progress", certID)
 	}
-	cm.lock[certID] = true
+	cm.locks[certID] = true
 	return nil
 }
 
-func (cm *CertManager) Unlock(certID string) {
+func (cm *CertManager) unlock(certID string) {
 	cm.muLock.Lock()
 	defer cm.muLock.Unlock()
-	delete(cm.lock, certID)
+	delete(cm.locks, certID)
 	return
 }
 
+// GenCSR does blah.
 func (cm *CertManager) GenCSR(params *pb.CSRParams) (*pb.CSR, error) {
+
 	return nil, nil
 }
 
+// Get does blah.
 func (cm *CertManager) Get() ([]*pb.CertificateInfo, error) {
-	cm.muCerts.Lock()
-	defer cm.muCerts.Unlock()
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
 	r := []*pb.CertificateInfo{}
 	for certID, cert := range cm.certs {
 		r = append(r, &pb.CertificateInfo{
 			CertificateId: certID,
 			Certificate: &pb.Certificate{
 				Type:        pb.CertificateType_CT_X509,
-				Certificate: cert.Raw, // needs encoding !!!!!!!!!!!!!!!!
+				Certificate: EncodeCert(cert),
 			},
 			ModificationTime: cm.certsModTime[certID].UnixNano(),
 		})
@@ -215,27 +258,112 @@ func (cm *CertManager) Get() ([]*pb.CertificateInfo, error) {
 	return r, nil
 }
 
+// Install does blah.
 func (cm *CertManager) Install(r *pb.LoadCertificateRequest) error {
+	certID := r.CertificateId
+	if err := cm.lock(certID); err != nil {
+		return err
+	}
+	defer cm.unlock(certID)
+
+	certificate, err := DecodeCert(r.Certificate.Certificate)
+	if err != nil {
+		return fmt.Errorf("failed to decode Certificate: %v", err)
+	}
+	cm.mu.Lock()
+	cm.mu.Unlock()
+	if _, ok := cm.certs[certID]; ok {
+		return fmt.Errorf("certificate id %q already exists", certID)
+	}
+	cm.certs[certID] = certificate
+	cm.certsModTime[certID] = nowTime()
+
+	if r.CaCertificate != nil && len(r.CaCertificate) != 0 {
+		certificates, err := DecodeCerts(toSlices(r.CaCertificate))
+		if err != nil {
+			return fmt.Errorf("failed to decode CA Bundle: %v", err)
+		}
+		cm.caBundle = certificates
+	}
+
 	return nil
 }
+
+// Revoke does blah.
 func (cm *CertManager) Revoke(req *pb.RevokeCertificatesRequest) (*pb.RevokeCertificatesResponse, error) {
-	cm.muCerts.Lock()
-	defer cm.muCerts.Unlock()
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
 	resp := &pb.RevokeCertificatesResponse{
 		RevokedCertificateId:       []string{},
 		CertificateRevocationError: []*pb.CertificateRevocationError{},
 	}
 	for _, certID := range req.CertificateId {
+		if cm.locked(certID) {
+			revErr := &pb.CertificateRevocationError{
+				CertificateId: certID,
+				ErrorMessage:  "an operation with this certID is in progress",
+			}
+			resp.CertificateRevocationError = append(resp.CertificateRevocationError, revErr)
+			continue
+		}
 		if _, ok := cm.certs[certID]; ok {
 			delete(cm.certs, certID)
 			delete(cm.certsModTime, certID)
 			resp.RevokedCertificateId = append(resp.RevokedCertificateId, certID)
 			continue
 		}
-		// add to failed
+		revErr := &pb.CertificateRevocationError{
+			CertificateId: certID,
+			ErrorMessage:  "does not exist",
+		}
+		resp.CertificateRevocationError = append(resp.CertificateRevocationError, revErr)
 	}
-	return nil, nil
+	return resp, nil
 }
-func (cm *CertManager) Rotate(r *pb.LoadCertificateRequest) (func(), error) {
-	return func() {}, nil
+
+// Rotate does blah.
+func (cm *CertManager) Rotate(r *pb.LoadCertificateRequest) (func(), func(), error) {
+	certID := r.CertificateId
+	if err := cm.lock(certID); err != nil {
+		return nil, nil, err
+	}
+
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
+	oldCert, ok := cm.certs[certID]
+	if !ok {
+		return nil, nil, fmt.Errorf("certificate ID %q does not exist", certID)
+	}
+	certificate, err := DecodeCert(r.Certificate.Certificate)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to decode Certificate: %v", err)
+	}
+	cm.certs[certID] = certificate
+	cm.certsModTime[certID] = nowTime()
+
+	var oldCABundle []*x509.Certificate
+	if r.CaCertificate != nil && len(r.CaCertificate) != 0 {
+		certificates, err := DecodeCerts(toSlices(r.CaCertificate))
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to decode CA Bundle: %v", err)
+		}
+		oldCABundle = cm.caBundle
+		cm.caBundle = certificates
+	}
+
+	rollback := func() {
+		cm.mu.Lock()
+		defer cm.mu.Unlock()
+		cm.certs[certID] = oldCert
+		if oldCABundle != nil {
+			cm.caBundle = oldCABundle
+		}
+		cm.unlock(certID)
+	}
+
+	accept := func() {
+		cm.unlock(certID)
+	}
+
+	return accept, rollback, nil
 }
--- gnoi/utils.go
+++ gnoi/utils.go
@@ -1,18 +1,59 @@
 package gnoi
 
 import (
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
 	"strings"
 
 	"github.com/google/go-cmp/cmp"
 )
 
+// FilterInternalPB returns true for protobuf internal variables in a path.
 func FilterInternalPB(p cmp.Path) bool {
 	return strings.Contains(p.String(), "XXX")
 }
 
+// Equal checks if two data structures are equal, ignoring protobuf internal variables.
 func Equal(x, y interface{}) bool {
 	return cmp.Equal(x, y, cmp.FilterPath(FilterInternalPB, cmp.Ignore()))
 }
+
+// Diff returns the diff between two data structures, ignoring protobuf internal variables.
 func Diff(x, y interface{}) string {
 	return cmp.Diff(x, y, cmp.FilterPath(FilterInternalPB, cmp.Ignore()))
+}
+
+// EncodeCert encodes a x509.Certificate into a PEM block.
+func EncodeCert(cert *x509.Certificate) []byte {
+	return pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: cert.Raw,
+	})
+}
+
+// DecodeCert decodes a PEM block into a x509.Certificate.
+func DecodeCert(bytes []byte) (*x509.Certificate, error) {
+	certDERBlock, _ := pem.Decode(bytes)
+	if certDERBlock == nil {
+		return nil, fmt.Errorf("failed to decode PEM block")
+	}
+	certificate, err := x509.ParseCertificate(certDERBlock.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode DER bytes")
+	}
+	return certificate, nil
+}
+
+// DecodeCerts decodes a slice of PEM blocks into a slice of x509.Certificate.
+func DecodeCerts(certs [][]byte) ([]*x509.Certificate, error) {
+	res := []*x509.Certificate{}
+	for _, cert := range certs {
+		decoded, err := DecodeCert(cert)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, decoded)
+	}
+	return res, nil
 }
