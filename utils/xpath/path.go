89f51f0ce1e24323174626e8d236623b2d12f09b
Sam Ribeiro <ribeiro@google.com>
2018-12-20 18:24:27 +0100

    Revert "Added support for parsing the origin in the Path. Checked for ':' in path attribute"
    This reverts commit 6659717a29a808fdccb8dad45015c4d3b0a75e99.

--- utils/xpath/path.go
+++ utils/xpath/path.go
@@ -18,7 +18,6 @@ package xpath
 
 import (
 	"fmt"
-	"strings"
 
 	pb "github.com/openconfig/gnmi/proto/gnmi"
 )
@@ -41,11 +40,6 @@ import (
 //    elem: <name: "state" >
 //    elem: <name: "counters" >
 func ToGNMIPath(xpath string) (*pb.Path, error) {
-	var origin string
-	if strings.ContainsAny(xpath, ":") {
-		splits := strings.Split(xpath, ":")
-		origin, xpath = splits[0], splits[1]
-	}
 	xpathElements, err := ParseStringPath(xpath)
 	if err != nil {
 		return nil, err
@@ -68,5 +62,5 @@ func ToGNMIPath(xpath string) (*pb.Path, error) {
 			return nil, fmt.Errorf("wrong data type: %T", v)
 		}
 	}
-	return &pb.Path{Origin: origin, Elem: pbPathElements}, nil
+	return &pb.Path{Elem: pbPathElements}, nil
 }
