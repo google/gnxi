513eb8c1af18eb14104c16a8a0c05defd8b79a0c
Sam Ribeiro <ribeiro@google.com>
2018-12-20 18:24:27 +0100

    Revert "Added support for parsing the origin in the Path. Checked for ':' in path attribute"
    This reverts commit 6659717a29a808fdccb8dad45015c4d3b0a75e99.

--- gnmi_set/gnmi_set.go
+++ gnmi_set/gnmi_set.go
@@ -59,11 +59,11 @@ var (
 func buildPbUpdateList(pathValuePairs []string) []*pb.Update {
 	var pbUpdateList []*pb.Update
 	for _, item := range pathValuePairs {
-		splitIndex := strings.LastIndexAny(item, ":")
-		if splitIndex < 1 {
+		pathValuePair := strings.SplitN(item, ":", 2)
+		// TODO (leguo): check if any path attribute contains ':'
+		if len(pathValuePair) != 2 || len(pathValuePair[1]) == 0 {
 			log.Exitf("invalid path-value pair: %v", item)
 		}
-		pathValuePair := []string{item[:splitIndex], item[(splitIndex + 1):]}
 		pbPath, err := xpath.ToGNMIPath(pathValuePair[0])
 		if err != nil {
 			log.Exitf("error in parsing xpath %q to gnmi path", pathValuePair[0])
