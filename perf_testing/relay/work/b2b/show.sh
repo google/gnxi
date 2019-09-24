75aeb8a1087db723b63a7b175e3f705ebda39208
river8 <chuan.han.comm@gmail.com>
2019-09-24 18:00:10 +0000

    Add perf scripts on relay machine

---
+++ perf_testing/relay/work/b2b/show.sh
@@ -0,0 +1,3 @@
+cat /sys/kernel/debug/qat_c6xx_0000\:03\:00.0/fw_counters | grep "Requests.*0]"
+cat /sys/kernel/debug/qat_c6xx_0000\:04\:00.0/fw_counters | grep "Requests.*0]"
+cat /sys/kernel/debug/qat_c6xx_0000\:05\:00.0/fw_counters | grep "Requests.*0]"
---
+++ perf_testing/relay/work/relay_ixia/dm_carl/mount.sh
@@ -0,0 +1,2 @@
+mount -t overlay -o lowerdir=/etc,upperdir=etc,workdir=etc_workdir none /etc
+mount -t overlay -o lowerdir=/var/run,upperdir=var_run,workdir=var_run_workdir none /var/run
