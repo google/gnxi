eb6c276bb9bbe0222874b49845f0145e0466fa82
Eric Moynihan <git@moynihan.io>
2020-07-01 10:31:10 +0100

    Implemented fixes

--- gnoi/reset/server.go
+++ gnoi/reset/server.go
@@ -7,19 +7,19 @@ import (
 	"google.golang.org/grpc"
 )
 
-// Settings for configurable options in Server
+// Settings for configurable options in Server.
 type Settings struct {
-	errorIfZero   bool
-	osUnsupported bool
+	ZeroFillUnsupported  bool
+	FactoryOSUnsupported bool
 }
 
-// Server for factory_reset service
+// Server for factory_reset service.
 type Server struct {
 	pb.FactoryResetServer
 	*Settings
 }
 
-// NewServer generates a new factory reset server
+// NewServer generates a new factory reset server.
 func NewServer(settings *Settings) *Server {
 	return &Server{Settings: settings}
 }
@@ -29,21 +29,17 @@ func (s *Server) Register(g *grpc.Server) {
 	pb.RegisterFactoryResetServer(g, s)
 }
 
-// Start rpc will start the factory reset process
+// Start rpc will start the factory reset process.
 func (s *Server) Start(ctx context.Context, req *pb.StartRequest) (*pb.StartResponse, error) {
-	resError := &pb.StartResponse_ResetError{}
-	if req.ZeroFill && s.errorIfZero {
-		resError.ResetError = &pb.ResetError{ZeroFillUnsupported: true}
+	resetError := &pb.ResetError{}
+	resetError.ZeroFillUnsupported = req.ZeroFill && s.ZeroFillUnsupported
+	resetError.FactoryOsUnsupported = req.FactoryOs && s.FactoryOSUnsupported
+
+	if resetError.ZeroFillUnsupported || resetError.FactoryOsUnsupported {
+		return &pb.StartResponse{Response: &pb.StartResponse_ResetError{ResetError: resetError}}, nil
 	}
-	if req.FactoryOs && s.osUnsupported {
-		if resError.ResetError != nil {
-			resError.ResetError.FactoryOsUnsupported = true
-		} else {
-			resError.ResetError = &pb.ResetError{FactoryOsUnsupported: true}
-		}
-	}
-	if resError.ResetError != nil {
-		return &pb.StartResponse{Response: resError}, nil
-	}
+
+	// TODO: Reset the target device
+
 	return &pb.StartResponse{Response: &pb.StartResponse_ResetSuccess{}}, nil
 }
--- gnoi/reset/server_test.go
+++ gnoi/reset/server_test.go
@@ -2,53 +2,79 @@ package reset
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/google/gnxi/gnoi/reset/pb"
 )
 
-func TestStart(t *testing.T) {
-	testSettings := []Settings{
-		{false, false},
-		{false, true},
-		{true, false},
-		{true, true},
-	}
+type test struct {
+	name     string
+	request  *pb.StartRequest
+	settings Settings
+}
 
-	testRequests := []pb.StartRequest{
+func makeTests() []test {
+	reqs := []pb.StartRequest{
 		{ZeroFill: true},
 		{ZeroFill: true, FactoryOs: true},
 		{FactoryOs: true},
 		{},
 	}
+	sets := []Settings{
+		{false, false},
+		{false, true},
+		{true, false},
+		{true, true},
+	}
+	t := []test{}
+	for _, set := range sets {
+		for _, req := range reqs {
+			name := fmt.Sprintf(
+				"ZF:{Given:%v, Unsupported:%v},OS:{Given:%v,Unsupported:%v}",
+				req.ZeroFill,
+				set.ZeroFillUnsupported,
+				req.FactoryOs,
+				set.FactoryOSUnsupported,
+			)
+			t = append(t, test{name: name, request: &req, settings: set})
+		}
+	}
+	return t
+}
 
-	for _, test := range testSettings {
-		s := NewServer(&test)
-		for _, testReq := range testRequests {
-			resp, _ := s.Start(context.Background(), &testReq)
+func TestStart(t *testing.T) {
+	tests := makeTests()
+	for _, test := range tests {
+		s := NewServer(&test.settings)
+		t.Run(test.name, func(t *testing.T) {
+			resp, err := s.Start(context.Background(), test.request)
+			if err != nil {
+				t.Fatalf("ops")
+			}
 			switch response := resp.Response.(type) {
 			case *pb.StartResponse_ResetSuccess:
-				if test.errorIfZero && testReq.ZeroFill || test.osUnsupported && testReq.FactoryOs {
+				if test.settings.ZeroFillUnsupported && test.request.ZeroFill || test.settings.FactoryOSUnsupported && test.request.FactoryOs {
 					t.Errorf(
 						"Error occured on case: \nSettings{errIfZero:%v,osUnsupported:%v}\nRequest{ZeroFill:%v,FactoryOs:%v} \nResponseSuccess{%v},",
-						test.errorIfZero,
-						test.osUnsupported,
-						testReq.ZeroFill,
-						testReq.FactoryOs,
+						test.settings.ZeroFillUnsupported,
+						test.settings.FactoryOSUnsupported,
+						test.request.ZeroFill,
+						test.request.FactoryOs,
 						response)
 				}
 			case *pb.StartResponse_ResetError:
-				if response.ResetError.ZeroFillUnsupported != (test.errorIfZero && testReq.ZeroFill) ||
-					response.ResetError.FactoryOsUnsupported != (test.osUnsupported && testReq.FactoryOs) {
+				if response.ResetError.ZeroFillUnsupported != (test.settings.ZeroFillUnsupported && test.request.ZeroFill) ||
+					response.ResetError.FactoryOsUnsupported != (test.settings.FactoryOSUnsupported && test.request.FactoryOs) {
 					t.Errorf(
 						"Error occured on case: \nSettings{errIfZero:%v,osUnsupported:%v}\nRequest{ZeroFill:%v,FactoryOs:%v} \nResponseError{%v},",
-						test.errorIfZero,
-						test.osUnsupported,
-						testReq.ZeroFill,
-						testReq.FactoryOs,
+						test.settings.ZeroFillUnsupported,
+						test.settings.FactoryOSUnsupported,
+						test.request.ZeroFill,
+						test.request.FactoryOs,
 						response)
 				}
 			}
-		}
+		})
 	}
 }
