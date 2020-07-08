bc36a752577bb4557a4dbeb614cc2b132647c711
Eric Moynihan <git@moynihan.io>
2020-07-08 10:29:10 +0100

    Added gNOI Client Activate RPC caller (#118)
    * Added gnoi Client Activate RPC caller
    
    * Added gNOI Activate client test
    
    * Added Licenses to OS client
    
    * Fixed details to only return upon unspecified
    
    * Added non nill check for test
    
    * Remove empty bin
    
    * Simplify test case
    
    * Simplified OS client

---
+++ gnoi/os/client.go
@@ -0,0 +1,57 @@
+/* Copyright 2020 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package os
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/gnxi/gnoi/os/pb"
+	"google.golang.org/grpc"
+)
+
+// Client handles requesting OS RPCs.
+type Client struct {
+	client pb.OSClient
+}
+
+// NewClient returns a new OS service client.
+func NewClient(c *grpc.ClientConn) *Client {
+	return &Client{client: pb.NewOSClient(c)}
+}
+
+// Activate invokes the Activate RPC for the OS service.
+func (c *Client) Activate(ctx context.Context, version string) error {
+	out, err := c.client.Activate(ctx, &pb.ActivateRequest{Version: version})
+	if err != nil {
+		return err
+	}
+	switch out.Response.(type) {
+	case *pb.ActivateResponse_ActivateOk:
+		return nil
+	case *pb.ActivateResponse_ActivateError:
+		res := out.GetActivateError()
+		switch res.GetType() {
+		case pb.ActivateError_UNSPECIFIED:
+			return fmt.Errorf("Unspecified ActivateError: %s", res.GetDetail())
+		case pb.ActivateError_NON_EXISTENT_VERSION:
+			return fmt.Errorf("Non existent version: %s", version)
+		default:
+			return fmt.Errorf("Unknown ActivateError: %s", res.GetType())
+		}
+	}
+	return nil
+}
---
+++ gnoi/os/client_test.go
@@ -0,0 +1,93 @@
+/* Copyright 2020 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package os
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/gnxi/gnoi/os/pb"
+	"google.golang.org/grpc"
+)
+
+type activateRPC func(ctx context.Context, in *pb.ActivateRequest, opts ...grpc.CallOption) (*pb.ActivateResponse, error)
+
+type mockClient struct {
+	pb.OSClient
+	activate activateRPC
+}
+
+type activateTest struct {
+	name    string
+	client  *mockClient
+	wantErr bool
+}
+
+func (c *mockClient) Activate(ctx context.Context, in *pb.ActivateRequest, opts ...grpc.CallOption) (*pb.ActivateResponse, error) {
+	return c.activate(ctx, in, opts...)
+}
+
+func activateErrorRPC(errType pb.ActivateError_Type, detail string) activateRPC {
+	return func(ctx context.Context, in *pb.ActivateRequest, opts ...grpc.CallOption) (*pb.ActivateResponse, error) {
+		return &pb.ActivateResponse{
+			Response: &pb.ActivateResponse_ActivateError{
+				ActivateError: &pb.ActivateError{Type: errType, Detail: detail},
+			}}, nil
+	}
+}
+
+func activateSuccessRPC(ctx context.Context, in *pb.ActivateRequest, opts ...grpc.CallOption) (*pb.ActivateResponse, error) {
+	return &pb.ActivateResponse{
+		Response: &pb.ActivateResponse_ActivateOk{}}, nil
+}
+
+func generateActivateTests() []activateTest {
+	tests := []activateTest{
+		{
+			"Success",
+			&mockClient{activate: activateSuccessRPC},
+			false,
+		},
+		{
+			"Unspecified",
+			&mockClient{activate: activateErrorRPC(pb.ActivateError_UNSPECIFIED, "detail")},
+			true,
+		},
+		{
+			"Non Existent Version",
+			&mockClient{activate: activateErrorRPC(pb.ActivateError_NON_EXISTENT_VERSION, "")},
+			true,
+		},
+	}
+	return tests
+}
+
+func TestActivate(t *testing.T) {
+	activateTests := generateActivateTests()
+	for _, test := range activateTests {
+		t.Run(test.name, func(t *testing.T) {
+			client := Client{client: test.client}
+			got := client.Activate(context.Background(), "version")
+			if test.wantErr {
+				if got == nil {
+					t.Error("want error, got nil")
+				}
+			} else if got != nil {
+				t.Errorf("want <nil>, got: %v", got)
+			}
+		})
+	}
+}
