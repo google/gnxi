0fd17fa84dc19780530a281f66f887c178cd0c35
Jared Mullin <jaredmullin@google.com>
2020-07-21 13:10:05 +0100

    Added test for ResetTarget

--- gnoi/reset/client_test.go
+++ gnoi/reset/client_test.go
@@ -15,12 +15,29 @@ limitations under the License.
 package reset
 
 import (
+	"context"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"google.golang.org/grpc"
 
 	"github.com/google/gnxi/gnoi/reset/pb"
 )
+
+type mockClient struct {
+	server Settings
+}
+
+func (m *mockClient) Start(ctx context.Context, request *pb.StartRequest, opts ...grpc.CallOption) (*pb.StartResponse, error) {
+	resetError := &pb.ResetError{}
+	resetError.ZeroFillUnsupported = request.ZeroFill && m.server.ZeroFillUnsupported
+	resetError.FactoryOsUnsupported = request.FactoryOs && m.server.FactoryOSUnsupported
+
+	if resetError.ZeroFillUnsupported || resetError.FactoryOsUnsupported {
+		return &pb.StartResponse{Response: &pb.StartResponse_ResetError{ResetError: resetError}}, nil
+	}
+	return &pb.StartResponse{Response: &pb.StartResponse_ResetSuccess{}}, nil
+}
 
 func TestCheckResponse(t *testing.T) {
 	tests := []struct {
@@ -86,9 +103,52 @@ func TestCheckResponse(t *testing.T) {
 	}
 	for _, test := range tests {
 		got := CheckResponse(test.response)
-		diff := cmp.Diff(test.want, got)
-		if diff != "" {
+		if diff := cmp.Diff(test.want, got); diff != "" {
 			t.Errorf("CheckResponse(%s): (-want +got):\n%s", test.response, diff)
+		}
+	}
+}
+
+func TestResetTarget(t *testing.T) {
+	requests := []*pb.StartRequest{
+		{},
+		{FactoryOs: true},
+		{ZeroFill: true},
+		{
+			FactoryOs: true,
+			ZeroFill:  true,
+		},
+	}
+	settings := []Settings{
+		{},
+		{FactoryOSUnsupported: true},
+		{ZeroFillUnsupported: true},
+		{
+			FactoryOSUnsupported: true,
+			ZeroFillUnsupported:  true,
+		},
+	}
+	for _, setting := range settings {
+		cli := &Client{client: &mockClient{server: setting}}
+		for _, req := range requests {
+			var expect *ResetError
+			got := cli.ResetTarget(context.Background(), req.ZeroFill, req.FactoryOs)
+			if (setting.FactoryOSUnsupported && req.FactoryOs) && (setting.ZeroFillUnsupported && req.ZeroFill) {
+				expect = &ResetError{
+					Msgs: []string{"Factory OS Rollback Unsupported", "Zero Filling Persistent Storage Unsupported"},
+				}
+			} else if setting.FactoryOSUnsupported && req.FactoryOs {
+				expect = &ResetError{
+					Msgs: []string{"Factory OS Rollback Unsupported"},
+				}
+			} else if setting.ZeroFillUnsupported && req.ZeroFill {
+				expect = &ResetError{
+					Msgs: []string{"Zero Filling Persistent Storage Unsupported"},
+				}
+			}
+			if diff := cmp.Diff(expect, got); diff != "" {
+				t.Errorf("ResetTarget(context.Background(), %v, %v): (-want +got)\n%s", req.ZeroFill, req.FactoryOs, diff)
+			}
 		}
 	}
 }
