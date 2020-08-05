1edb78f6cddf36663479612ee74770329acd661f
Eric Moynihan <git@moynihan.io>
2020-08-05 12:42:52 +0100

    Added test for dockerfile

--- gnxi_tester/config/dockerfiles.go
+++ gnxi_tester/config/dockerfiles.go
@@ -1,3 +1,18 @@
+/* Copyright 2020 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package config
 
 import (
@@ -28,7 +43,7 @@ CMD [ "tail", "-f", "/dev/null" ]
 `
 
 func createDockerfiles(tests Tests) string {
-	homeDir, err := os.UserHomeDir()
+	homeDir, err := userHome()
 	if err != nil {
 		log.Errorf("couldn't get home directory: %v", err)
 	}
@@ -39,7 +54,11 @@ func createDockerfiles(tests Tests) string {
 	return filesDir
 }
 
-func safeWriteDockerfile(name, loc string) {
+var userHome = func() (string, error) {
+	return os.UserHomeDir()
+}
+
+var safeWriteDockerfile = func(name, loc string) {
 	_, existErr := os.Stat(loc)
 	if errors.Is(existErr, os.ErrNotExist) {
 		content := fmt.Sprintf(dockerfile, name, name, name, name)
