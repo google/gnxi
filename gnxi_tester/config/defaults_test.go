feff5b566c1dd14f275ad217fcbbcc06f6406c73
Jared Mullin <jaredmullin@google.com>
2020-08-05 12:00:49 +0100

    Fix for defaults_test.go

--- gnxi_tester/config/defaults_test.go
+++ gnxi_tester/config/defaults_test.go
@@ -8,28 +8,39 @@ import (
 )
 
 func TestGenerateTestCases(t *testing.T) {
-	got := generateTestCases()
-	if got == nil {
-		t.Errorf("Tests Not Defined!")
-	}
-	if got["gnoi_os"] == nil {
-		t.Errorf("OS Tests Not Defined!")
-	}
-	if got["gnoi_cert"] == nil {
-		t.Errorf("Certificate Tests Not Defined!")
-	}
-	if got["gnoi_reset"] == nil {
-		t.Errorf("Reset Tests Not Defined!")
+	got, order := generateTestCases()
+	for _, testName := range order {
+		if _, ok := got[testName]; !ok {
+			t.Errorf("Wanted %s, but not retrieved!", testName)
+		}
 	}
 }
 
 func TestSetDefaults(t *testing.T) {
 	viper.Reset()
-	var got map[string][]Test
-	want := generateTestCases()
+	var testsGot map[string][]Test
+	var orderGot []string
+	testsWanted, orderWanted := generateTestCases()
+	want := struct {
+		tests map[string][]Test
+		order []string
+	}{
+		tests: testsWanted,
+		order: orderWanted,
+	}
 	setDefaults()
-	if err := viper.UnmarshalKey("tests", &got); err != nil {
+	if err := viper.UnmarshalKey("tests", &testsGot); err != nil {
 		t.Errorf("Error getting tests: %v", err)
+	}
+	if err := viper.UnmarshalKey("order", &orderGot); err != nil {
+		t.Errorf("Error getting order: %v", err)
+	}
+	got := struct {
+		tests map[string][]Test
+		order []string
+	}{
+		tests: testsGot,
+		order: orderGot,
 	}
 	if diff := pretty.Compare(want, got); diff != "" {
 		t.Errorf("GetTests(): (-want +got):\n%s", diff)
