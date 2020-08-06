c58665d358be829510a58e59198f044cbdd24246
Jared Mullin <jaredmullin@google.com>
2020-08-06 14:47:56 +0100

    Changed regex in default tests
    More regex fixes

--- gnxi_tester/config/defaults.go
+++ gnxi_tester/config/defaults.go
@@ -55,7 +55,7 @@ func generateTestCases() (Tests, []string) {
 			Name:  "Get certs",
 			Args:  map[string]string{"op": "get"},
 			Wait:  10,
-			Wants: `GetCertificates:\n{.*}$`,
+			Wants: `GetCertificates:\n{\w*`,
 		},
 		{
 			Name:   "Install a cert",
@@ -71,7 +71,7 @@ func generateTestCases() (Tests, []string) {
 		{
 			Name:       "Revoke a cert",
 			Args:       map[string]string{"op": "revoke", "cert_id": "&<new_cert_id>"},
-			Wants:      `RevokeCertificates:\n{.*}`,
+			Wants:      `RevokeCertificates:\n{\w*`,
 			DoesntWant: "Failed",
 		},
 		{
@@ -91,7 +91,7 @@ func generateTestCases() (Tests, []string) {
 		{
 			Name:  "Resetting a Target Successfully",
 			Args:  map[string]string{},
-			Wants: `^$`,
+			Wants: `Reset Called Successfully!`,
 		},
 	}
 	osTests := []Test{
