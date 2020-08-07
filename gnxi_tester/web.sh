36f099c4ae880a1aabd4ee2fb02ab3470243b6e4
Eric Moynihan <git@moynihan.io>
2020-08-07 15:35:17 +0100

    Addded web ui boilerplate and docker dev environment

---
+++ gnxi_tester/web.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Shallow clone gnxi 
+git clone \
+    --depth 1 \
+    https://github.com/google/gnxi.git
+;
+cd gnxi/gnxi_tester
+
+docker-compose up -d && 
+    echo "Web UI running on http://localhost:8080. Type 'docker ps' to see more info on running containers."
