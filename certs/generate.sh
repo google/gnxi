b694a9446a996d247f34850bad4829021725386e
Eric Moynihan <git@moynihan.io>
2020-08-14 11:52:49 +0100

    Added generator for certs

--- certs/generate.sh
+++ certs/generate.sh
@@ -1,21 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 rm -f *.key *.csr *.crt *.pem *.srl
 
 SUBJ="/C=NZ/ST=Test/L=Test/O=Test/OU=Test/CN=ca"
+SAN="subjectAltName = DNS:ca"
 
 # Generate CA Private Key
 openssl req \
         -newkey rsa:2048 \
         -nodes \
         -keyout ca.key \
-        -subj $SUBJ
+        -subj $SUBJ \
+        -reqexts SAN \
+        -config <(cat /etc/ssl/openssl.cnf \
+                <(printf "\n[SAN]\n$SAN"))
 
 # Generate Req
 openssl req \
         -key ca.key \
         -new -out ca.csr \
-        -subj $SUBJ
+        -subj $SUBJ \
+        -reqexts SAN \
+        -config <(cat /etc/ssl/openssl.cnf \
+                <(printf "\n[SAN]\n$SAN"))
 
 # Generate self signed x509
 openssl x509 \
@@ -25,19 +32,26 @@ openssl x509 \
         -days 365 -out ca.crt 
 
 SUBJ="/C=NZ/ST=Test/L=Test/O=Test/OU=Test/CN=target.com"
+SAN="subjectAltName = DNS:target.com"
 
 # Generate Target Private Key
 openssl req \
         -newkey rsa:2048 \
         -nodes \
         -keyout target.key \
-        -subj $SUBJ
+        -subj $SUBJ \
+        -reqexts SAN \
+        -config <(cat /etc/ssl/openssl.cnf \
+                <(printf "\n[SAN]\n$SAN"))
 
 # Generate Req
 openssl req \
         -key target.key \
         -new -out target.csr \
-        -subj $SUBJ
+        -subj $SUBJ \
+        -reqexts SAN \
+        -config <(cat /etc/ssl/openssl.cnf \
+                <(printf "\n[SAN]\n$SAN"))
 
 # Generate x509 with signed CA
 openssl x509 \
@@ -49,19 +63,26 @@ openssl x509 \
         -out target.crt
 
 SUBJ="/C=NZ/ST=Test/L=Test/O=Test/OU=Test/CN=client.com"
+SAN="subjectAltName = DNS:client.com"
 
 # Generate Client Private Key
 openssl req \
         -newkey rsa:2048 \
         -nodes \
         -keyout client.key \
-        -subj $SUBJ
+        -subj $SUBJ \
+        -reqexts SAN \
+        -config <(cat /etc/ssl/openssl.cnf \
+                <(printf "\n[SAN]\n$SAN"))
 
 # Generate Req
 openssl req \
         -key client.key \
         -new -out client.csr \
-        -subj $SUBJ
+        -subj $SUBJ \
+        -reqexts SAN \
+        -config <(cat /etc/ssl/openssl.cnf \
+                <(printf "\n[SAN]\n$SAN"))
 
 # Generate x509 with signed CA
 openssl x509 \
