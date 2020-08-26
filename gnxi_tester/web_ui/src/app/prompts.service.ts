01979f825f9f64a005f272894d8ab2489f93c873
Jared Mullin <jaredmullin@google.com>
2020-08-26 11:52:00 +0100

    Tidy prompts service imports

--- gnxi_tester/web_ui/src/app/prompts.service.ts
+++ gnxi_tester/web_ui/src/app/prompts.service.ts
@@ -1,8 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { PromptsSet, PromptsList, Prompts } from './models/Prompts';
 import { environment } from './environment';
+import { Prompts, PromptsList, PromptsSet } from './models/Prompts';
 
 
 @Injectable({
