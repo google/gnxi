072cc656de1744e0cd94e7ec6a31abe8d7461a19
Eric Moynihan <git@moynihan.io>
2020-09-08 13:04:48 +0100

    Created tests for cert/server.Rotate

--- gnoi/cert/server_test.go
+++ gnoi/cert/server_test.go
@@ -100,6 +100,232 @@ func (s *mockInstallServer) Recv() (*pb.InstallCertificateRequest, error) {
 	}
 }
 
+type mockRotateServer struct {
+	pb.CertificateManagement_RotateServer
+	reqMap  []*rotateRequestMap
+	i       int
+	recv    chan int
+	recvErr chan error
+}
+
+func (s *mockRotateServer) Send(resp *pb.RotateCertificateResponse) error {
+	if s.i < len(s.reqMap) {
+		if reflect.TypeOf(resp.RotateResponse) == reflect.TypeOf(s.reqMap[s.i].resp.RotateResponse) {
+			s.recv <- s.i
+		} else {
+			s.recvErr <- errors.New("error")
+		}
+		s.i++
+	}
+	return nil
+}
+
+func (s *mockRotateServer) Recv() (*pb.RotateCertificateRequest, error) {
+	select {
+	case i := <-s.recv:
+		return s.reqMap[i].req, nil
+	case err := <-s.recvErr:
+		return nil, err
+	}
+}
+
+func TestTargeRotate(t *testing.T) {
+	tests := []struct {
+		name   string
+		reqMap []*rotateRequestMap
+		err    error
+	}{
+		{
+			"expected GenerateCSRRequest, got something else",
+			[]*rotateRequestMap{
+				{
+					req:  nil,
+					resp: nil,
+				},
+			},
+			errors.New("expected GenerateCSRRequest, got something else"),
+		},
+		{
+			"certificate not supported",
+			[]*rotateRequestMap{
+				{
+					req: &pb.RotateCertificateRequest{
+						RotateRequest: &pb.RotateCertificateRequest_GenerateCsr{GenerateCsr: &pb.GenerateCSRRequest{CsrParams: &pb.CSRParams{}}},
+					},
+					resp: nil,
+				},
+			},
+			errors.New("certificate type \"CT_UNKNOWN\" not supported"),
+		},
+		{
+			"key not supported",
+			[]*rotateRequestMap{
+				{
+					req: &pb.RotateCertificateRequest{
+						RotateRequest: &pb.RotateCertificateRequest_GenerateCsr{GenerateCsr: &pb.GenerateCSRRequest{CsrParams: &pb.CSRParams{Type: 1}}},
+					},
+					resp: nil,
+				},
+			},
+			errors.New("key type \"KT_UNKNOWN\" not supported"),
+		},
+		{
+			"failed to receive RotateCertificateRequest",
+			[]*rotateRequestMap{
+				{
+					req: &pb.RotateCertificateRequest{
+						RotateRequest: &pb.RotateCertificateRequest_GenerateCsr{GenerateCsr: &pb.GenerateCSRRequest{CsrParams: &pb.CSRParams{Type: 1, KeyType: 1}}},
+					},
+				},
+				{
+					resp: &pb.RotateCertificateResponse{},
+				},
+			},
+			errors.New("failed to receive RotateCertificateRequest: error"),
+		},
+		{
+			"expected LoadCertificateRequest, got something else",
+			[]*rotateRequestMap{
+				{
+					req: &pb.RotateCertificateRequest{
+						RotateRequest: &pb.RotateCertificateRequest_GenerateCsr{GenerateCsr: &pb.GenerateCSRRequest{CsrParams: &pb.CSRParams{Type: 1, KeyType: 1}}},
+					},
+				},
+				{
+					resp: &pb.RotateCertificateResponse{
+						RotateResponse: &pb.RotateCertificateResponse_GeneratedCsr{},
+					},
+				},
+			},
+			errors.New("expected LoadCertificateRequest, got something else"),
+		},
+		{
+			"unexpected Certificate type",
+			[]*rotateRequestMap{
+				{
+					req: &pb.RotateCertificateRequest{
+						RotateRequest: &pb.RotateCertificateRequest_GenerateCsr{GenerateCsr: &pb.GenerateCSRRequest{CsrParams: &pb.CSRParams{Type: 1, KeyType: 1}}},
+					},
+				},
+				{
+					resp: &pb.RotateCertificateResponse{
+						RotateResponse: &pb.RotateCertificateResponse_GeneratedCsr{},
+					},
+					req: &pb.RotateCertificateRequest{
+						RotateRequest: &pb.RotateCertificateRequest_LoadCertificate{LoadCertificate: &pb.LoadCertificateRequest{Certificate: &pb.Certificate{Type: 0}}},
+					},
+				},
+			},
+			errors.New("unexpected Certificate type: 0"),
+		},
+		{
+			"rolling back - failed to receive RotateCertificateRequest",
+			[]*rotateRequestMap{
+				{
+					req: &pb.RotateCertificateRequest{
+						RotateRequest: &pb.RotateCertificateRequest_GenerateCsr{GenerateCsr: &pb.GenerateCSRRequest{CsrParams: &pb.CSRParams{Type: 1, KeyType: 1}}},
+					},
+				},
+				{
+					resp: &pb.RotateCertificateResponse{
+						RotateResponse: &pb.RotateCertificateResponse_GeneratedCsr{},
+					},
+					req: &pb.RotateCertificateRequest{
+						RotateRequest: &pb.RotateCertificateRequest_LoadCertificate{LoadCertificate: &pb.LoadCertificateRequest{
+							Certificate:    &pb.Certificate{Type: 1},
+							CaCertificates: []*pb.Certificate{{Type: pb.CertificateType_CT_X509}}},
+						},
+					},
+				},
+				{
+					resp: &pb.RotateCertificateResponse{
+						RotateResponse: &pb.RotateCertificateResponse_GeneratedCsr{},
+					},
+				},
+			},
+			errors.New("rolling back - failed to receive RotateCertificateRequest: error"),
+		},
+		{
+			"expected FinalizeRequest, got something else",
+			[]*rotateRequestMap{
+				{
+					req: &pb.RotateCertificateRequest{
+						RotateRequest: &pb.RotateCertificateRequest_GenerateCsr{GenerateCsr: &pb.GenerateCSRRequest{CsrParams: &pb.CSRParams{Type: 1, KeyType: 1}}},
+					},
+				},
+				{
+					resp: &pb.RotateCertificateResponse{
+						RotateResponse: &pb.RotateCertificateResponse_GeneratedCsr{},
+					},
+					req: &pb.RotateCertificateRequest{
+						RotateRequest: &pb.RotateCertificateRequest_LoadCertificate{LoadCertificate: &pb.LoadCertificateRequest{
+							Certificate:    &pb.Certificate{Type: 1},
+							CaCertificates: []*pb.Certificate{{Type: pb.CertificateType_CT_X509}}},
+						},
+					},
+				},
+				{
+					resp: &pb.RotateCertificateResponse{
+						RotateResponse: &pb.RotateCertificateResponse_LoadCertificate{},
+					},
+				},
+			},
+			errors.New("expected FinalizeRequest, got something else"),
+		},
+		{
+			"terminate",
+			[]*rotateRequestMap{
+				{
+					req: &pb.RotateCertificateRequest{
+						RotateRequest: &pb.RotateCertificateRequest_GenerateCsr{GenerateCsr: &pb.GenerateCSRRequest{CsrParams: &pb.CSRParams{Type: 1, KeyType: 1}}},
+					},
+				},
+				{
+					resp: &pb.RotateCertificateResponse{
+						RotateResponse: &pb.RotateCertificateResponse_GeneratedCsr{},
+					},
+					req: &pb.RotateCertificateRequest{
+						RotateRequest: &pb.RotateCertificateRequest_LoadCertificate{LoadCertificate: &pb.LoadCertificateRequest{
+							Certificate:    &pb.Certificate{Type: 1},
+							CaCertificates: []*pb.Certificate{{Type: pb.CertificateType_CT_X509}}},
+						},
+					},
+				},
+				{
+					resp: &pb.RotateCertificateResponse{
+						RotateResponse: &pb.RotateCertificateResponse_LoadCertificate{},
+					},
+					req: &pb.RotateCertificateRequest{
+						RotateRequest: &pb.RotateCertificateRequest_FinalizeRotation{FinalizeRotation: &pb.FinalizeRequest{}},
+					},
+				},
+			},
+			nil,
+		},
+	}
+	mmi := &mockManagerInterface{
+		mockGenCSR: func(p pkix.Name) ([]byte, error) {
+			return []byte{}, nil
+		},
+		mockRotate: func(string, []byte, [][]byte) (func(), func(), error) {
+			return func() {}, func() {}, nil
+		},
+	}
+	s := NewServer(mmi)
+	for _, test := range tests {
+		stream := &mockRotateServer{
+			i:       1,
+			reqMap:  test.reqMap,
+			recv:    make(chan int, 1),
+			recvErr: make(chan error, 1),
+		}
+		stream.recv <- 0
+		if err := s.Rotate(stream); fmt.Sprintf("%v", err) != fmt.Sprintf("%v", test.err) {
+			t.Errorf("wanted err(%v), got(%v)", test.err, err)
+		}
+	}
+}
+
 func TestTargetInstall(t *testing.T) {
 	tests := []struct {
 		name   string
