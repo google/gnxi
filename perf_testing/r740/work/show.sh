5f44df13a119bcc3748fa15b216c38bb9072a1d8
Sam Ribeiro <ribeiro@google.com>
2021-11-30 16:53:45 +0100

    Delete perf_testing directory
    This is old and not needed anymore.

--- perf_testing/r740/work/show.sh
+++
@@ -1,3 +0,0 @@
-cat /sys/kernel/debug/qat_c6xx_0000\:3d\:00.0/fw_counters | grep "Requests.*0]"
-cat /sys/kernel/debug/qat_c6xx_0000\:3f\:00.0/fw_counters | grep "Requests.*0]"
-cat /sys/kernel/debug/qat_c6xx_0000\:41\:00.0/fw_counters | grep "Requests.*0]"
