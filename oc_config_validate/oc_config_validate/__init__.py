google/gnxi

commit 39a5b33eac41aff21f778348665a843060c605e3

Log to STDOUT the Number and Name of the test run. (#324)
This makes it easier to find the failing/passing tests in the YAML file.

Also, format and linting corrections.

oc_config_validate/oc_config_validate/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "2.3.0"
+__version__ = "2.3.1"

oc_config_validate/oc_config_validate/runner.py
@@ -64,14 +64,9 @@ class TestThread(threading.Thread):
         text_test_runner.failfast = self.stop_on_error
         if self.conn_lock:
             self.conn_lock.acquire()
-        logging.info("Running Test '%s'", self.test_suite.test_name)
         start_time = int(time.time())
         self.results = text_test_runner.run(self.test_suite)
         self.duration_sec = int(time.time() - start_time)
-        logging.info("Test '%s' took %d secs: %s\n",
-                     self.test_suite.test_name,
-                     self.duration_sec,
-                     "PASS" if self.results.wasSuccessful() else "FAIL")
         if self.conn_lock:
             self.conn_lock.release()
         self.results.test_class_name = self.test_suite.test_class_name
@@ -159,7 +154,8 @@ def _getClassFromName(class_name: str) -> Optional[testbase.TestCase]:
         for part in class_name.split('.'):
             cls = getattr(cls, part)
     except AttributeError as err:
-        raise TestClassError(f"Unable to find test class {class_name}") from err
+        raise TestClassError(
+            f"Unable to find test class {class_name}") from err
     if not issubclass(cls, testbase.TestCase):
         raise TestClassError(
             f"Test class {class_name} not derived from test_testbase.TestCase")
@@ -189,11 +185,17 @@ def runTests(ctx: context.TestContext,
         logging.info("Stopping if a Test fails.")
     results = []
     conn_lock = threading.Lock()
-    for suite in suites:
+    for i, suite in enumerate(suites):
         thread = TestThread(suite, conn_lock, stop_on_error)
+        logging.debug(f"Running Test {i+1} '{suite.test_name}'")
         thread.start()
         thread.join()
         results.append(thread.results)
+        logging.info(
+            f"Test {i+1} '{suite.test_name}' took "
+            f"{thread.results.duration_sec} secs: "
+            f"{'NOT ' if thread.results.wasSuccessful() else ''}"
+            "PASSED\n")
         if stop_on_error and not thread.results.wasSuccessful():
             break
     return results
@@ -225,7 +227,7 @@ def setInitConfigs(ctx: context.TestContext,
             schema.parsePath(init_config.xpath)
             tgt.gNMISetConfigFile(init_config.filename, init_config.xpath)
             logging.info(f"Initial OpenConfig '{init_config.filename}' "
-                         "applied at {init_config.xpath}")
+                         f"applied at {init_config.xpath}")
         except (IOError, json.JSONDecodeError, target.BaseError) as err:
             msg = (f"Unable to set configuration '{init_config.filename}' "
                    f"at '{init_config.xpath}': {err}")

oc_config_validate/oc_config_validate/testcases/telemetry_sample.py
@@ -111,10 +111,9 @@ class CheckLeafs(SubsSampleTestCase):
 
     def testSubscribeSample(self):
         """"""
-        self.assertArgs(["model"])
+        self.assertArgs(["model", "xpath"])
 
         self.assertModelXpath(self.model, self.xpath)
-        want_paths = schema.ocLeafsPaths(self.model, self.xpath)
 
         self.subscribeSample()
 

commit d2d971324f961d0986b22e8d9163c85520e4a1cc

Code improvements for oc_config_validate (#325)
* Code improvements for oc_config_validate

 * Move the validation of the Target values to where they are defined (in Context)
 * Use a ContextManager to ensure gNMI Connection and Close to the Target

* Factorize code that sends gNMI Gets and asserts.

This avoids code repetition in the testcases, as it is very common to
send gNMI Get requests and assert the reply, before further tests.

* Correct log text error introduced in PR #324

Log messages NOT PASSED when ! .wasSuccess()

oc_config_validate/oc_config_validate/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "2.3.1"
+__version__ = "2.3.2"

oc_config_validate/oc_config_validate/__main__.py
@@ -221,43 +221,35 @@ def main():  # noqa
     logging.info("Read tests file '%s': %d tests to run",
                  args["tests_file"], len(ctx.tests))
 
-    if not ctx.target:
-        ctx.target = context.Target()
-
     # Override Target options
     for arg in ["target", "username", "password", "no_tls", "private_key",
                 "cert_chain", "root_ca_cert", "tls_host_override",
                 "target_cert_as_root_ca", "gnmi_set_cooldown_secs"]:
         if args[arg]:
             setattr(ctx.target, arg, args[arg])
-
-    tgt = target.TestTarget(ctx.target)
     try:
-        tgt.validate()
+        ctx.target.validate()
     except ValueError as error:
         sys.exit("Invalid Target: %s" % error)
 
-    logging.info("Testing gNMI Target %s.", tgt)
-
-    if tgt.gnmi_set_cooldown_secs:
-        logging.info("Using gNMI Set Cooldown of %d secs",
-                     tgt.gnmi_set_cooldown_secs)
-
-    # Apply initial configuration
+    # Concatenate initial configuration paths and files
     if args["init_config_file"]:
         ctx.init_configs.append(context.InitConfig(args["init_config_file"],
                                                    args["init_config_xpath"]))
-    try:
-        runner.setInitConfigs(ctx, tgt,
-                              stop_on_error=args["stop_on_error"])
-    except runner.InitConfigError as err:
-        sys.exit("Unable to apply init config(s): %s" % err)
 
-    start_t = time.time()
-    results = runner.runTests(ctx, tgt, stop_on_error=args["stop_on_error"])
-    end_t = time.time()
+    with target.TestTarget(ctx.target) as tgt:
+        try:
+            runner.setInitConfigs(ctx, tgt,
+                                  stop_on_error=args["stop_on_error"])
+        except runner.InitConfigError as err:
+            sys.exit("Unable to apply init config(s): %s" % err)
 
-    test_run = testbase.TestRun(ctx)
+        start_t = time.time()
+        results = runner.runTests(
+            ctx, tgt, stop_on_error=args["stop_on_error"])
+        end_t = time.time()
+
+    test_run = testbase.TestRun(ctx, tgt)
     test_run.copyResults(results, start_t, end_t)
     logging.info("Results Summary: %s", test_run.summary())
 

oc_config_validate/oc_config_validate/context.py
@@ -91,6 +91,21 @@ class Target(yaml.YAMLObject):
     def __repr__(self):
         return 'Target(target=%r, no_tls=%r)' % (self.target, self.no_tls)
 
+    def validate(self):
+        """Ensures the Target is defined appropriately.
+
+        Raises:
+            ValueError when the Target is not defined correctly.
+        """
+        parts = self.target.split(":")
+        if len(parts) != 2 or not bool(parts[0]) or not parts[1].isdigit():
+            raise ValueError("Needed valid target HOSTNAME:PORT")
+
+        # If using client certificates for TLS, provide key and cert
+        if not self.no_tls and (
+                bool(self.private_key) ^ bool(self.cert_chain)):
+            raise ValueError("TLS key and cert are both needed.")
+
 
 def fromFile(file_path) -> TestContext:
     """Create a TestContext object from a YAML file.
@@ -104,4 +119,10 @@ def fromFile(file_path) -> TestContext:
            file.
     """
     with open(file_path, encoding="utf8") as raw_profile_data:
-        return yaml.safe_load(raw_profile_data)
+        ctx = yaml.safe_load(raw_profile_data)
+
+    # If no Target is defined in the tests file, create a default one.
+    if not ctx.target:
+        ctx.target = Target()
+
+    return ctx

oc_config_validate/oc_config_validate/runner.py
@@ -194,7 +194,7 @@ def runTests(ctx: context.TestContext,
         logging.info(
             f"Test {i+1} '{suite.test_name}' took "
             f"{thread.results.duration_sec} secs: "
-            f"{'NOT ' if thread.results.wasSuccessful() else ''}"
+            f"{'' if thread.results.wasSuccessful() else 'NOT '}"
             "PASSED\n")
         if stop_on_error and not thread.results.wasSuccessful():
             break

oc_config_validate/oc_config_validate/target.py
@@ -49,8 +49,10 @@ class RpcError(BaseError):
 class TestTarget():
     """gNMI Target to run tests against.
 
-    Args:
-        hostname_port: Target to connect to, as 'hostname:port'.
+    Example:
+        with target.TestTarget(ctx.tgt) as tgt:
+            tgt.gNMISet()
+            tgt.gNMIGet()
     """
 
     def __init__(self, tgt: context.Target):
@@ -105,29 +107,36 @@ class TestTarget():
         if root_ca:
             self.root_ca = _fileReadOrNone(root_ca)
 
-    def _gNMIConnnect(self):
-        """Create a gNMI connection."""
-        if self.stub:
-            return
+    def gNMIConnect(self):
+        """Create a gNMI connection to the target."""
         if self.notls:
-            self.channel = gnmi_pb2_grpc.grpc.insecure_channel(self.address)
+            self.channel = grpc.insecure_channel(self.address)
         else:
             creds = self._buildCredentials()
             logging.debug("creds: %s", creds)
             if self.host_tls_override:
-                self.channel = gnmi_pb2_grpc.grpc.secure_channel(
+                self.channel = grpc.secure_channel(
                     self.address, creds,
                     (('grpc.ssl_target_name_override',
                       self.host_tls_override,),))
             else:
-                self.channel = gnmi_pb2_grpc.grpc.secure_channel(
+                self.channel = grpc.secure_channel(
                     self.address, creds)
         self.stub = gnmi_pb2_grpc.gNMIStub(self.channel)
 
     def gNMIClose(self):
+        """Closes the gNMI connection to the target."""
         self.channel.close()
+        self.stub = None
 
-    def _buildCredentials(self) -> gnmi_pb2_grpc.grpc.ssl_channel_credentials:
+    def __enter__(self):
+        self.gNMIConnect()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.gNMIClose()
+
+    def _buildCredentials(self) -> grpc.ssl_channel_credentials:
         """Build credentials used in gNMI Requests.
 
         Returns:
@@ -146,12 +155,12 @@ class TestTarget():
                 raise TargetError(
                     "Unable to get Root CA from Target") from err
 
-        return gnmi_pb2_grpc.grpc.ssl_channel_credentials(
+        return grpc.ssl_channel_credentials(
             root_certificates=self.root_ca,
             private_key=self.key,
             certificate_chain=self.cert)
 
-    def _GnmiStubAuth(self) -> List[Tuple[str, str]]:
+    def _GnmiAuthMetadata(self) -> List[Tuple[str, str]]:
         """Returns the gNMI metadata used for authentication."""
         if self.username:  # User/pass supplied for Authentication.
             return [('username', self.username), ('password', self.password)]
@@ -170,12 +179,10 @@ class TestTarget():
             RpcError if unable to connect to Target.
         """
         path = schema.parsePath(xpath)
-        self._gNMIConnnect()
-        auth = self._GnmiStubAuth()
         try:
             return self.stub.Get(
                 gnmi_pb2.GetRequest(path=[path], encoding='JSON_IETF'),
-                metadata=auth)
+                metadata=self._GnmiAuthMetadata())
         except _InactiveRpcError as err:
             raise RpcError(err) from err
 
@@ -203,9 +210,8 @@ class TestTarget():
         if value:
             val = schema.pythonToTypedValue(value)
             path_val = gnmi_pb2.Update(path=path, val=val)
-        self._gNMIConnnect()
-        auth = self._GnmiStubAuth()
 
+        auth = self._GnmiAuthMetadata()
         response = None
         try:
             if set_type.lower() == 'delete':
@@ -302,8 +308,8 @@ class TestTarget():
             GnmiError if no sync_response was received, when
                 check_sync_response.
         """
-        self._gNMIConnnect()
-        auth = self._GnmiStubAuth()
+
+        auth = self._GnmiAuthMetadata()
         notifications = []
         got_sync_response = False
         try:
@@ -356,17 +362,3 @@ class TestTarget():
         request = schema.gNMISubscriptionStreamSampleRequest(
             [xpath], sample_interval)
         return self._gNMISubscribe(request, timeout=timeout)
-
-    def validate(self):
-        """Ensures the Target is defined appropriately.
-
-        Raises:
-          ValueError when the Target is not defined correctly.
-        """
-        parts = self.address.split(":")
-        if len(parts) != 2 or not bool(parts[0]) or not parts[1].isdigit():
-            raise ValueError("Needed valid target HOSTNAME:PORT")
-
-        # If using client certificates for TLS, provide key and cert
-        if not self.notls and (bool(self.key) ^ bool(self.cert)):
-            raise ValueError("TLS key and cert are both needed.")

oc_config_validate/oc_config_validate/testbase.py
@@ -154,6 +154,58 @@ class TestCase(unittest.case.TestCase):
             logging.info(*msg)
         return resp_val
 
+    def gNMIGetAssertJson(self, xpath: str) -> Union[str, bytes]:
+        """Send a gNMI Get and asserts that the response is a JSON text.
+
+        Args:
+            xpath:  gNMI Path.
+
+        Returns:
+            a JSON text.
+
+        Raises:
+          AssertionError if unable to gNMI Get or if the response is not JSON.
+        """
+        resp = self.gNMIGetAssertGet(xpath)
+        resp_val = resp.json_ietf_val
+        self.assertIsNotNone(resp_val,
+                             "The gNMI GET response is not JSON IETF")
+        return resp_val
+
+    def gNMIGetAssertBool(self, xpath: str) -> bool:
+        """Send a gNMI Get and asserts that the response is a bool value.
+
+        Args:
+            xpath:  gNMI Path.
+
+        Returns:
+            a boolean value.
+
+        Raises:
+          AssertionError if unable to gNMI Get or if the response is not bool.
+        """
+        resp = self.gNMIGetAssertGet(xpath)
+        self.assertIsNotNone(
+            resp.bool_val,
+            "The gNMI Get response is not a boolean: %s" % resp)
+        return resp
+
+    def gNMIGetAssertGet(self, xpath: str) -> gnmi_pb2.TypedValue:
+        """Send a gNMI Get and asserts that there is a response.
+
+        Args:
+            xpath:  gNMI Path.
+
+        Returns:
+            the response to the Get requets.
+
+        Raises:
+          AssertionError if unable to gNMI Get or if the response is None.
+        """
+        resp = self.gNMIGet(xpath)
+        self.assertIsNotNone(resp, "No gNMI GET response")
+        return resp
+
     def gNMISetUpdate(self, xpath: str, value: Any) -> bool:
         """Send a gNMI Set Update message to the test's target
 
@@ -325,25 +377,6 @@ class TestCase(unittest.case.TestCase):
             match, error = False, err
         self.assertTrue(match, "%s: %s" % (msg, error))
 
-    def gNMIGetJson(self, xpath: str) -> Union[str, bytes]:
-        """Send a gNMI Get and asserts that the response is a JSON text.
-
-        Args:
-            xpath:  gNMI Path.
-
-        Returns:
-            a JSON text.
-
-        Raises:
-          AssertionError if unable to gNMI Get or if the response is not JSON.
-        """
-        resp = self.gNMIGet(xpath)
-        self.assertIsNotNone(resp, "No gNMI GET response")
-        resp_val = resp.json_ietf_val
-        self.assertIsNotNone(resp_val,
-                             "The gNMI GET response is not JSON IETF")
-        return resp_val
-
     def assertJsonCmp(self, json_value: Union[str, bytes], want: dict):
         """Assert that the JSON response matches the wanted values..
 
@@ -510,7 +543,7 @@ class TestRun():
     tests_total = 0
     tests_fail = 0
 
-    def __init__(self, ctx: context.TestContext):
+    def __init__(self, ctx: context.TestContext, tgt: target.TestTarget):
         self.test_target = ctx.target.target
         if ctx.description:
             self.description = ctx.description

oc_config_validate/oc_config_validate/testcases/config_state.py
@@ -41,7 +41,7 @@ class SetConfigCheckState(testbase.TestCase):
     def _get_check_retry(self):
         # gNMI Get on /config
         xpath = self.xpath.rstrip("/") + "/config"
-        resp_val = self.gNMIGetJson(xpath)
+        resp_val = self.gNMIGetAssertJson(xpath)
         model = schema.ocContainerFromPath(self.model, xpath)
         self.assertJsonModel(resp_val, model,
                              "Get /config does not match the model")

oc_config_validate/oc_config_validate/testcases/get.py
@@ -19,7 +19,7 @@ class Get(testbase.TestCase):
         """"""
         self.assertArgs(["xpath"])
         self.assertXpath(self.xpath)
-        self.gNMIGet(self.xpath)
+        self.gNMIGetAssertGet(self.xpath)
 
 
 class GetCompare(testbase.TestCase):
@@ -40,8 +40,7 @@ class GetCompare(testbase.TestCase):
         """"""
         self.assertArgs(["xpath", "want"])
         self.assertXpath(self.xpath)
-        resp_val = self.gNMIGet(self.xpath)
-        self.assertIsNotNone(resp_val, "No gNMI GET response")
+        resp_val = self.gNMIGetAssertGet(self.xpath)
         got = schema.typedValueToPython(resp_val)
         self.assertEqual(type(got), type(self.want),
                          "Values of different types")
@@ -71,11 +70,7 @@ class GetJsonCheck(testbase.TestCase):
         self.assertArgs(["xpath", "model"])
         self.assertXpath(self.xpath)
         self.assertModelXpath(self.model, self.xpath)
-        resp = self.gNMIGet(self.xpath)
-        self.assertIsNotNone(resp, "No gNMI GET response")
-        resp_val = resp.json_ietf_val
-        self.assertIsNotNone(resp_val,
-                             "The gNMI GET response is not JSON IETF")
+        resp_val = self.gNMIGetAssertJson(self.xpath)
         model = schema.ocContainerFromPath(self.model, self.xpath)
         self.assertJsonModel(resp_val, model,
                              "Get response JSON does not match model")
@@ -104,11 +99,7 @@ class GetJsonCheckCompare(testbase.TestCase):
         self.assertModelXpath(self.model, self.xpath)
         self.assertIsInstance(self.want_json, dict,
                               "'want_json' is not a valid JSON object")
-        resp = self.gNMIGet(self.xpath)
-        self.assertIsNotNone(resp, "No gNMI GET response")
-        resp_val = resp.json_ietf_val
-        self.assertIsNotNone(resp_val,
-                             "The gNMI GET response is not JSON IETF")
+        resp_val = self.gNMIGetAssertJson(self.xpath)
         model = schema.ocContainerFromPath(self.model, self.xpath)
         self.assertJsonModel(resp_val, model,
                              "Get response does not match the model")

oc_config_validate/oc_config_validate/testcases/setget.py
@@ -51,11 +51,7 @@ class SetGetJsonCheck(TestCase):
     @testbase.retryAssertionError
     def testSetGetJsonCheck(self):
         """"""
-        resp = self.gNMIGet(self.xpath)
-        self.assertIsNotNone(resp, "No gNMI GET response")
-        self.resp_val = resp.json_ietf_val
-        self.assertIsNotNone(self.resp_val,
-                             "The gNMI GET response is not JSON IETF")
+        self.resp_val = self.gNMIGetAssertJson(self.xpath)
         model = schema.ocContainerFromPath(self.model, self.xpath)
         self.assertJsonModel(self.resp_val, model,
                              "Get response JSON does not match the model")
@@ -80,11 +76,7 @@ class SetGetJsonCheckCompare(TestCase):
 
     @testbase.retryAssertionError
     def testSetGetJsonCheckCompare(self):
-        resp = self.gNMIGet(self.xpath)
-        self.assertIsNotNone(resp, "No gNMI GET response")
-        self.resp_val = resp.json_ietf_val
-        self.assertIsNotNone(self.resp_val,
-                             "The gNMI GET response is not JSON IETF")
+        self.resp_val = self.gNMIGetAssertJson(self.xpath)
         model = schema.ocContainerFromPath(self.model, self.xpath)
         self.assertJsonModel(self.resp_val, model,
                              "Get response JSON does not match the model")

oc_config_validate/oc_config_validate/testcases/static_route.py
@@ -57,7 +57,7 @@ class AddStaticRoute(testbase.TestCase):
         xpath = ("/local-routes/static-routes/static[prefix=%s]/"
                  "next-hops/next-hop[index=%d]/config") % (
             self.prefix, self.index)
-        resp_val = self.gNMIGetJson(xpath)
+        resp_val = self.gNMIGetAssertJson(xpath)
         self.assertJsonModel(
             resp_val, self.nh.config,
             'gNMI Get on the /config container does not match model')
@@ -71,7 +71,7 @@ class AddStaticRoute(testbase.TestCase):
         xpath = ("/local-routes/static-routes/static[prefix=%s]/"
                  "next-hops/next-hop[index=%d]/state") % (
             self.prefix, self.index)
-        resp_val = self.gNMIGetJson(xpath)
+        resp_val = self.gNMIGetAssertJson(xpath)
         self.nh.state._set_index(self.index)
         self.nh.state._set_next_hop(self.next_hop)
         if self.metric is not None:
@@ -172,7 +172,7 @@ class CheckRouteState(testbase.TestCase):
         ste._set_next_hop(self.next_hop)
         if self.metric is not None:
             ste._set_metric(self.metric)
-        resp_val = self.gNMIGetJson(xpath)
+        resp_val = self.gNMIGetAssertJson(xpath)
         self.assertJsonModel(
             resp_val, ste,
             'gNMI Get on the /state container does not match model')
@@ -216,7 +216,7 @@ class CheckRouteConfig(testbase.TestCase):
         cfg._set_next_hop(self.next_hop)
         if self.metric is not None:
             cfg._set_metric(self.metric)
-        resp_val = self.gNMIGetJson(xpath)
+        resp_val = self.gNMIGetAssertJson(xpath)
         self.assertJsonModel(
             resp_val, cfg,
             'gNMI Get on the /config container does not match model')

oc_config_validate/oc_config_validate/testcases/subif_ip.py
@@ -46,28 +46,20 @@ class SetSubifDhcp(testbase.TestCase):
         xpath = ("/interfaces/interface[name=%s]/subinterfaces/"
                  "subinterface[index=%d]/ipv4/config/dhcp-client") % (
             self.interface, self.index)
-        resp_val = self.gNMIGet(xpath)
-        self.assertIsNotNone(resp_val, "No gNMI GET response")
-        self.assertIsNotNone(
-            resp_val.bool_val,
-            "The gNMI Get response is not a boolean: %s" % resp_val)
-        self.assertEqual(resp_val.bool_val, self.dhcp,
+        resp_val = self.gNMIGetAssertBool(xpath)
+        self.assertEqual(resp_val, self.dhcp,
                          "Dhcp client config = %s, wanted %s" % (
-                             resp_val.bool_val, self.dhcp))
+                             resp_val, self.dhcp))
 
     @retry(exceptions=AssertionError, tries=5, delay=15)
     def test0300(self):
         xpath = ("/interfaces/interface[name=%s]/subinterfaces/"
                  "subinterface[index=%d]/ipv4/state/dhcp-client") % (
             self.interface, self.index)
-        resp_val = self.gNMIGet(xpath)
-        self.assertIsNotNone(resp_val, "No gNMI GET response")
-        self.assertIsNotNone(
-            resp_val.bool_val,
-            "The gNMI Get response is not a boolean: %s" % resp_val)
-        self.assertEqual(resp_val.bool_val, self.dhcp,
+        resp_val = self.gNMIGetAssertBool(xpath)
+        self.assertEqual(resp_val, self.dhcp,
                          "Dhcp client state = %s, wanted %s" % (
-                             resp_val.bool_val, self.dhcp))
+                             resp_val, self.dhcp))
 
 
 class CheckSubifDhcpState(testbase.TestCase):
@@ -92,14 +84,10 @@ class CheckSubifDhcpState(testbase.TestCase):
         xpath = ("/interfaces/interface[name=%s]/subinterfaces/"
                  "subinterface[index=%d]/ipv4/state/dhcp-client") % (
             self.interface, self.index)
-        resp_val = self.gNMIGet(xpath)
-        self.assertIsNotNone(resp_val, "No gNMI GET response")
-        self.assertIsNotNone(
-            resp_val.bool_val,
-            "The gNMI Get response is not a boolean: %s" % resp_val)
-        self.assertEqual(resp_val.bool_val, self.dhcp,
+        resp_val = self.gNMIGetAssertBool(xpath)
+        self.assertEqual(resp_val, self.dhcp,
                          "Dhcp client state = %s, wanted %s" % (
-                             resp_val.bool_val, self.dhcp))
+                             resp_val, self.dhcp))
 
 
 class CheckSubifDhcpConfig(testbase.TestCase):
@@ -125,14 +113,10 @@ class CheckSubifDhcpConfig(testbase.TestCase):
         xpath = ("/interfaces/interface[name=%s]/subinterfaces/"
                  "subinterface[index=%d]/ipv4/config/dhcp-client") % (
             self.interface, self.index)
-        resp_val = self.gNMIGet(xpath)
-        self.assertIsNotNone(resp_val, "No gNMI GET response")
-        self.assertIsNotNone(
-            resp_val.bool_val,
-            "The gNMI Get response is not a boolean: %s" % resp_val)
-        self.assertEqual(resp_val.bool_val, self.dhcp,
+        resp_val = self.gNMIGetAssertBool(xpath)
+        self.assertEqual(resp_val, self.dhcp,
                          "Dhcp client config = %s, wanted %s" % (
-                             resp_val.bool_val, self.dhcp))
+                             resp_val, self.dhcp))
 
 
 class AddSubifIp(testbase.TestCase):
@@ -178,7 +162,7 @@ class AddSubifIp(testbase.TestCase):
                  "subinterface[index=%d]/ipv4/addresses"
                  "/address[ip=%s]/config") % (
             self.interface, self.index, self.address)
-        resp_val = self.gNMIGetJson(xpath)
+        resp_val = self.gNMIGetAssertJson(xpath)
         self.assertJsonModel(
             resp_val, self.addr.config,
             'gNMI Get on the /config container does not match model')
@@ -193,7 +177,7 @@ class AddSubifIp(testbase.TestCase):
                  "subinterface[index=%d]/ipv4/addresses"
                  "/address[ip=%s]/state") % (
             self.interface, self.index, self.address)
-        resp_val = self.gNMIGetJson(xpath)
+        resp_val = self.gNMIGetAssertJson(xpath)
 
         self.addr.state._set_ip(self.address)
         self.addr.state._set_prefix_length(self.prefix_length)
@@ -283,7 +267,7 @@ class CheckSubifIpState(testbase.TestCase):
                  "subinterface[index=%d]/ipv4/addresses/"
                  "address[ip=%s]/state") % (
             self.interface, self.index, self.address)
-        resp_val = self.gNMIGetJson(xpath)
+        resp_val = self.gNMIGetAssertJson(xpath)
         iface = oc_interfaces().interfaces.interface.add(self.interface)
         subif = iface.subinterfaces.subinterface.add(self.index)
         if self.index:
@@ -325,7 +309,7 @@ class CheckSubifIpConfig(testbase.TestCase):
                  "subinterface[index=%d]/ipv4/addresses/"
                  "address[ip=%s]/config") % (
             self.interface, self.index, self.address)
-        resp_val = self.gNMIGetJson(xpath)
+        resp_val = self.gNMIGetAssertJson(xpath)
         iface = oc_interfaces().interfaces.interface.add(self.interface)
         subif = iface.subinterfaces.subinterface.add(self.index)
         if self.index: